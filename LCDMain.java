f9e4f21ab17fe5ac406390f43f35a8c1d1b970de
lmpacent <73558668+lmpacent@users.noreply.github.com>
2021-07-09 15:22:29 -0600

    minor formatting fix

--- LCDMain.java
+++ LCDMain.java
@@ -1,28 +1,27 @@
-package LCD;
+package lcdmain;
 
 import com.phidget22.TemperatureSensor;
 import java.util.ArrayList;
 
 public class LCDMain {
-	      
-	public static void main(String args[]) throws Exception{
-		
-		TemperatureSensor temperatureSensor = new TemperatureSensor();
-		temperatureSensor.open(1000);
-		
-		//First argument is the number of data points displayed at 1 time on the graph
-		LCDPlot test = new LCDPlot(12, true);
-		
-		System.out.println("Start");
-		
-		while(true) {
-			
-			test.getData().add(temperatureSensor.getTemperature());
-			test.start();
-			Thread.sleep(350);
-		
-		}
 
-	}
-       
+    public static void main(String args[]) throws Exception {
+
+        TemperatureSensor temperatureSensor = new TemperatureSensor();
+        temperatureSensor.open(1000);
+
+        //First argument is the number of data points displayed at 1 time on the graph
+        LCDPlot test = new LCDPlot(12, true);
+
+        System.out.println("Start");
+
+        while (true) {
+
+            test.getData().add(temperatureSensor.getTemperature());
+            test.start();
+            Thread.sleep(350);
+
+        }
+
+    }
 }
