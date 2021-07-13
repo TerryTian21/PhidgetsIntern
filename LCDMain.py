30328fedbe9b4988398d1bf0e151eeb7ddf41b09
TerryTian21 <87080334+TerryTian21@users.noreply.github.com>
2021-07-13 14:02:05 -0600

    Update LCDMain.py

--- LCDMain.py
+++ LCDMain.py
@@ -1,6 +1,5 @@
 # Adding Phidgets Library
 from Phidget22.Devices.TemperatureSensor import *
-
 # Importing libraries used in program
 from LCDPlot import LCDPlot
 import time
@@ -14,14 +13,14 @@ temperatureSensor.openWaitForAttachment(1000)
 # Creating a new object which will plot the data from the temperature sensor
 # First argument is the number of data points displayed at 1 time on the graph
 # Second argument is whether user wants to record from sensor to a file
-graph = LCDPlot(10, True)
+test = LCDPlot(10, True)
 
 # Indication that program has started
-print("Start")
+print("start")
 
 # Continuous loop which collects the temperature every 250 milliseconds
 # Data is then passed to the test class and graphed/logged in file
 while True:
-    graph.setDataPoint(temperatureSensor.getTemperature())
-    graph.start()
+    test.set_data_point(temperatureSensor.getTemperature())
+    test.start()
     time.sleep(.250)
