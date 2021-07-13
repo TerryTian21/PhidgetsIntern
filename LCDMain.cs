b7823dfbad9b8957d3c80ada9d87503cd01cec93
TerryTian21 <87080334+TerryTian21@users.noreply.github.com>
2021-07-13 11:53:09 -0600

    Update LCDMain.cs

--- LCDMain.cs
+++ LCDMain.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+// Import Phidgets Library
 using Phidget22;
+
+// Import Libraries used in Program
+using System*;
+
 
 namespace PhidgetsIntern
 
@@ -16,13 +15,24 @@ namespace PhidgetsIntern
         public static void Main(String [] args)
         {
 
+            // Create temperature sensor
             TemperatureSensor temperatureSensor = new TemperatureSensor();
+
+            // Open
             temperatureSensor.Open(1000);
 
+
+            // Creating a new object which will plot the data from the temperature sensor
+            // First argument is the number of data points displayed at 1 time on the graph
+            // Second argument is whether user wants to record from sensor to a file
             LCDPlot test = new LCDPlot(12, true);
 
+            //Indication that program has started
             Console.WriteLine("Start");
 
+
+            // Continuous loop which collects the temperature every 250 milliseconds
+            // Data is then passed to the test class and graphed/logged in a file
             while (true)
             {
                 test.addDataPoint(temperatureSensor.Temperature);
@@ -33,8 +43,7 @@ namespace PhidgetsIntern
         }
 
 
-
     }
-        
-   
+
+
 }
