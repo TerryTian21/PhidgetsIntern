f3422085e9bb116fd7bd63eba68f1affacbd554e
TerryTian21 <87080334+TerryTian21@users.noreply.github.com>
2021-07-14 08:17:37 -0600

    Update LCDMain.java

--- LCDMain.java
+++ LCDMain.java
@@ -1,34 +1,37 @@
+package LCD;
 
 //Add Phidgets Library
-import com.phidget22.*;
+import com.phidget22.TemperatureSensor;
 
 public class LCDMain {
 
 	public static void main(String args[]) throws Exception {
-		
-		//Create Temperature Sensor
+
+		// Create Temperature Sensor
 		TemperatureSensor temperatureSensor = new TemperatureSensor();
-		
-		//Open
+
+		// Open
 		temperatureSensor.open(1000);
 
-		//Creating a new object which will plot the data from the temperature sensor
-		// First argument is the number of data points displayed at 1 time on the graph
-		// Second argument is whether user wants to record data from sensor to a file
+		/**
+		 * Creating a new object which will plot the data from the temperature sensor
+		 * First argument is the number of data points displayed at 1 time on the graph
+		 * Second argument is whether user wants to record data from sensor to a file
+		 **/
+
 		LCDPlot graph = new LCDPlot(12, true);
 
-		//Indication that program has started
+		// Indication that program has started
 		System.out.println("Start");
 
-		
-		//Continuous loop which collects the temperature every 250 milliseconds
-		//Data is then passed to the test class and graphed/logged in file
+		// Continuous loop which collects the temperature every 250 milliseconds
+		// Data is then passed to the test class and graphed/logged in file
 		while (true) {
 
 			graph.addDataPoint(temperatureSensor.getTemperature());
-			graph.start();
-			Thread.sleep(250);
+			Thread.sleep(temperatureSensor.getDataInterval());
 		}
 
 	}
 
+}
